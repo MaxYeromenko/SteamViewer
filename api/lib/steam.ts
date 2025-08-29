faa6027a2db4143d8fe2c4fd4e873c4264a8c0c8
MaxYeromenko <eromenko.max@gmail.com>
2025-08-29 23:55:47 +0300

    a

--- api/lib/steam.ts
+++ api/lib/steam.ts
@@ -30,20 +30,24 @@ export async function fetchSteamUserData(steamid: string) {
         }));
     }
 
-    const gamesRes = await fetch(
-        `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamid}&include_appinfo=1&include_played_free_games=1`
-    );
-    const gamesData = await gamesRes.json();
-    const games = gamesData.response.games?.map((g: any) => ({
-        appid: g.appid,
-        name: g.name,
-        playtimeForever: g.playtime_forever,
-        playtime2Weeks: g.playtime_2weeks,
-        iconUrl: g.img_icon_url,
-        logoUrl: g.img_logo_url,
-    })) || [];
+    // const gamesRes = await fetch(
+    //     `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamid}&include_appinfo=1&include_played_free_games=1`
+    // );
+    // const gamesData = await gamesRes.json();
 
-    console.log(games);
+    // let games: any[] = [];
+    // if (gamesData.response.games?.length) {
+    //     games = gamesData.response.games.map((g: any) => ({
+    //         appid: g.appid,
+    //         name: g.name,
+    //         playtimeForever: g.playtime_forever,
+    //         playtime2Weeks: g.playtime_2weeks,
+    //         iconUrl: g.img_icon_url,
+    //         logoUrl: g.img_logo_url,
+    //     }));
+    // }
+
+    // console.log(games);
 
     return {
         steamid: player.steamid,
@@ -56,6 +60,6 @@ export async function fetchSteamUserData(steamid: string) {
         country: player.loccountrycode,
         timeCreated: player.timecreated,
         friends,
-        games,
+        // games,
     };
 }
